5781e44f00de78cecc4003b1824b18e7f1bceb34
Pradhyum R <drpradhyum2016@outlook.com>
2023-04-12 21:27:40 +0000

    Theoretically fixed push

--- retrod/list.h
+++ retrod/list.h
@@ -5,4 +5,4 @@
 #define CDR(ll) ll->next
 #define LOOP_THROUGH(type, var, ll) for (LIST(type) _i_ = ll, type var = CAR(_i_); (_i_ != NULL) && (var = CAR(_i_)); _i_ = CDR(_i_))   // Fix maybe
 #define LAST_ELEMENT(type, _ll) ({LIST(type) ll = _ll; while (CDR(ll) != NULL) ll = CDR(ll); ll;})   // gcc extension
-#define PUSH(type, ll, x) CDR(LAST_ELEMENT(type, ll)) = _LL_ELEMENT(x, NULL)
+#define PUSH(type, ll, x) do { if (CAR(ll) == NULL) CAR(ll) = _LL_ELEMENT(x, NULL); else CDR(LAST_ELEMENT(type, ll)) = _LL_ELEMENT(x, NULL); } while (0)
