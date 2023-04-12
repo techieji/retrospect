f1eccda415925b78e91d4d5c3fcc506919918c39
Pradhyum R <drpradhyum2016@outlook.com>
2023-04-12 21:23:45 +0000

    basic linked list lib

---
+++ retrod/list.h
@@ -0,0 +1,8 @@
+#define LIST(type) struct type##List
+#define DECLARE_LL(type) LIST(type) { type data; LIST(type)* next }
+#define _LL_ELEMENT(type, data, next) (LIST(type)) { data, next }
+#define CAR(ll) ll->data
+#define CDR(ll) ll->next
+#define LOOP_THROUGH(type, var, ll) for (LIST(type) _i_ = ll, type var = CAR(_i_); (_i_ != NULL) && (var = CAR(_i_)); _i_ = CDR(_i_))   // Fix maybe
+#define LAST_ELEMENT(type, _ll) ({LIST(type) ll = _ll; while (CDR(ll) != NULL) ll = CDR(ll); ll;})   // gcc extension
+#define PUSH(type, ll, x) CDR(LAST_ELEMENT(type, ll)) = _LL_ELEMENT(x, NULL)
